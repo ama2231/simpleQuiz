d93ce8461d954653b0d7b1e62f839ab0336d3d51
eafflu <edemakuaafflu@gmail.com>
2018-01-21 13:52:17 -0600

    Add 4/6 quiz questions to page

--- quiz.js
+++ quiz.js
@@ -65,27 +65,54 @@ $('button').click(function(){
         
     });
     $('<button>').addClass('btn btn-primary submit').text('Submit').appendTo('#quizBody')
-    $('.submit').click(() => {
-        console.log('been clicked')
-        // when the submit button is clicked empty the quiz body 
-    $('#quizBody').empty();
-    $('<h2>').text(quizObj[1].question).appendTo('#quizBody')
-    quizObj[1].asked++;
-    console.log('quiz body has been emptied', quizObj[1])
-    quizObj[1].answers.forEach((answer) => {
-       $('<div>').attr('id', 'label').appendTo('#quizBody')
-       $('<input>').attr('type', 'radio').attr('id', 'obj1').addClass('radio').appendTo('#label')
-       $('<label>').attr('for', 'obj1').text(answer).appendTo('#label')
-    });
-    $('<button>').addClass('btn btn-primary submit1').text('Submit').appendTo('#quizBody')
-        
 
-        // loop thru the quiz object and append the next question and answers
+        $('.submit').click(() => {
+                    console.log('been clicked')
+                    // when the submit button is clicked empty the quiz body 
+                    $('#quizBody').empty();
+                    $('<h2>').text(quizObj[1].question).appendTo('#quizBody')
+                    quizObj[1].asked++;
+                    console.log('quiz body has been emptied', quizObj[1])
+                    quizObj[1].answers.forEach((answer) => {
+                        $('<div>').attr('id', 'label').appendTo('#quizBody')
+                        $('<input>').attr('type', 'radio').attr('id', 'obj1').addClass('radio').appendTo('#label')
+                        $('<label>').attr('for', 'obj1').text(answer).appendTo('#label')
+                    });
+                    $('<button>').addClass('btn btn-primary submit1').text('Submit').appendTo('#quizBody')
+                    
+                    $('.submit1').click(() =>{
+                        console.log('been clicked', quizObj[2])
+                        $('#quizBody').empty();
+                        $('<h2>').text(quizObj[2].question).appendTo('#quizBody');
+                        quizObj[2].asked ++;
+                        quizObj[2].answers.forEach((answer) => {
+                            $('<div>').attr('id', 'label').appendTo('#quizBody')
+                            $('<input>').attr('type', 'radio').attr('id', 'obj2').addClass('radio').appendTo('#label')
+                            $('<label>').attr('for', 'obj2').text(answer).appendTo('#label')
+                        })
+                        
+                        $('<button>').addClass('btn btn-primary submit2').text('Submit').appendTo('#quizBody')
+                        $('.submit2').click(() => {
+                            console.log('been clicked', quizObj[3]);
+                        $('#quizBody').empty();
+                        $('<h2>').text(quizObj[3].question).appendTo('#quizBody');
+                        quizObj[3].asked ++
+                        quizObj[3].answers.forEach((answer) => {
+                            $('<div>').attr('id', 'label').appendTo('#quizBody')
+                            $('<input>').attr('type', 'radio').attr('id', 'obj3').addClass('radio').appendTo('#label')
+                            $('<label>').attr('for', 'obj3').text(answer).appendTo('#label')
+                        })
+                        $('<button>').addClass('btn btn-primary submit3').text('Submit').appendTo('#quizBody')
+                        
+                        
+                            
+                        })
+                    });
+                    
+
+                    });
+                    
+        
     })
-    
-    
-    
-})
-// when the submit button is clicked the next element should be displayed
 
 });
